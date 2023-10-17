b89a85c15bcad56816d4060d9dbf56cb563937fe
Douglas Carreira <lenusui123@gmail.com>
2023-10-17 05:11:50 -0300

    17/10/2023 - Criação dos Destaques

--- js/script.js
+++ js/script.js
@@ -2,3 +2,12 @@ $(".slider").slick({
     prevArrow: $("#arrowEsquerda"),
     nextArrow: $("#arrowDireita"),
 });
+
+$(".slider-dots").slick({
+    arrows: false,
+    dots: true,
+    appendDots: $(".slick-slider-dots"),
+    customPaging: function (slider, i) {
+        return '<div class="dots" id=' + i + "><div class=ativo></div></div>";
+    },
+});
